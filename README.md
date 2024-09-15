3dddbf84fcaf8ae92dc0f79449ceb37cdc4eb1c8
Ticiano <ticianoodonto@gmail.com>
2024-09-14 23:08:34 -0300

    Atualização
    Atualização da aula dia 11/09/24.

--- README.md
+++
@@ -1,3 +0,0 @@
-# TECNOLOGIA-PARA-BACK-END-AVANCADO
-Atividades da disciplina de Back-End Avançado, quarto período do curso.
-10/09/2024
---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,29 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+}
