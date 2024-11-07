980cc81f60deea391d4c2167a85ee2561a689550
Ticiano <ticianoodonto@gmail.com>
2024-11-06 23:34:15 -0300

    Atualização aula 05/11
    Atualização aula 05/11

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    AgendaDeConsultas agenda;
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid
                                   DadosAgendamentoConsulta dados) {
-        System.out.println(dados);
-
-        return null;
+        var dto = agenda.agendar(dados);
+        return ResponseEntity.ok(dto);
     }
 }
-
--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,15 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -15,8 +19,11 @@ public class AgendaDeConsultas {
     private MedicoRepository medicoRepository;
     @Autowired
     private PacienteRepository pacienteRepository;
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
 
-    public void agendar(DadosAgendamentoConsulta dados) {
+    public DadosDetalhamentoConsulta agendar(
+            DadosAgendamentoConsulta dados) {
 
         if (!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoException(
@@ -30,6 +37,12 @@ public class AgendaDeConsultas {
                     "Id do médico informado não existe!");
         }
 
+        var paciente = pacienteRepository.getReferenceById
+                (dados.idPaciente());
+        var medico = escolherMedico(dados);
+        var consulta = new Consulta(null, medico,
+                paciente, dados.data());
+        return new DadosDetalhamentoConsulta(consulta);
     }
 
     private Medico escolherMedico(
@@ -41,7 +54,7 @@ public class AgendaDeConsultas {
 
         if (dados.especialidade() == null) {
             throw new ValidacaoException(
-          "Especialidade é obrigatória quando médico não for escolhido!");
+                    "Especialidade é obrigatória quando médico não for escolhido!");
         }
         return medicoRepository.
                 escolherMedicoAleatorioLivreNaData(
