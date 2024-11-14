35f0274d32dab5fbfe64181781b434f348a49ad9
Ticiano <ticianoodonto@gmail.com>
2024-11-13 21:57:28 -0300

    atualização no código
    atualização no código

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorAgendamentoDeConsulta.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.domain.consulta.validacoes;
-
-import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-
-public interface ValidadorAgendamentoDeConsulta {
-    void validar(DadosAgendamentoConsulta dados);
-}
--- src/main/java/med/voll/api/usuario/DadosAutenticacao.java
+++
@@ -1,5 +0,0 @@
-package med.voll.api.usuario;
-public record DadosAutenticacao
-        (String login, String senha) {
-}
-
